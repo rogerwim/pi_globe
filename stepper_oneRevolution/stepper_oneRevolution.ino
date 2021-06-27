9c9eb319a4948a8f0fb2d740829b1b39809c0fb2
Roger van Bommel <f_vanbommel@yahoo.co.uk>
2021-06-27 14:10:15 +0200

    test

--- stepper_oneRevolution/stepper_oneRevolution.ino
+++ stepper_oneRevolution/stepper_oneRevolution.ino
@@ -14,39 +14,51 @@
  by Tom Igoe
 
  */
-
 #include <Stepper.h>
+float rot = 0.0;
+const int stepsPerRevolution = 1440;  // change this to fit the number of steps per revolution
+// for your motor
+
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+int home() {
+  if(analogRead(A0) > 0) {
+    return 0;  
+  }
+  int count = 0;
+  while(analogRead(A0) < 10) {
+    myStepper.step(1);
+    count++;
+  }
+  return count;
+}
+void step2() {
+  long ang = 360;
+  long steps = ang/(360/stepsPerRevolution);
+  Serial.println(steps);
+  myStepper.step(steps);
+}
 void stop() {
   digitalWrite(8,0);
   digitalWrite(9,0);
   digitalWrite(10,0);
   digitalWrite(11,0);
 }
-const int stepsPerRevolution = 1425;  // change this to fit the number of steps per revolution
-// for your motor
-
-// initialize the stepper library on pins 8 through 11:
-Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
-void home() {
-  int val = analogRead(A0);
-  while(val < 20) {
-    val = analogRead(A0);
-    myStepper.step(1);
-  }
-}
 void setup() {
   // set the speed at 60 rpm:
-  myStepper.setSpeed(10);
+  myStepper.setSpeed(7);
   // initialize the serial port:
-  Serial.begin(115200);
+  Serial.begin(9600);
   home();
 }
+
 void loop() {
-  delay(100);
-  myStepper.step(-70);
-  delay(100);
-  myStepper.step(100);
-  delay(100);
-  home();
-  stop();
+  // step one revolution  in one direction:
+  //int a = home();
+  //if(a != 0) {
+  //  Serial.println(a);  
+  //}
+  step2();
+  delay(1000);
+  //stop();
 }
