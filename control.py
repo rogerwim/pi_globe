f2b6c363f05c0e509637218264190c7552e9c96d
Your Name <you@example.com>
2022-03-25 19:16:33 +0000

    sync changes

--- control.py
+++ control.py
@@ -1,6 +1,9 @@
 import serial
 steps_per_rev = 1440
-arduino = serial.Serial('/dev/ttyUSB1',9600)
+arduino = ''
+def init(port):
+    global arduino
+    arduino = serial.Serial(port,9600)
 def bits_to_byte(bits):
     byte  = 0
     if type(bits) != list:
@@ -15,15 +18,16 @@ def bits_to_byte(bits):
 def send_command(command, data1,data2,data3,data4,data5,data6): # example command [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     command1 = 0
     command2 = 0
-    print(command)
+    #print(command)
     command1 = bits_to_byte(command[0:8])
     command2 = bits_to_byte(command[8:])
-    print(command1,command2)
+    #print(command1,command2)
     data_and_command = command1 + command2 + bytes([data1]) + bytes([data2]) + bytes([data3]) + bytes([data4]) + bytes([data5]) + bytes([data6])
-    print(data_and_command)
+    #print(data_and_command)
     arduino.write(data_and_command)
-    print(arduino.read())
+    return arduino.read()
 def step_with_home(ang):
+    print(ang)
     steps = (ang/360)*steps_per_rev
     print(steps)
     steps = int(steps)
@@ -33,18 +37,21 @@ def step_with_home(ang):
     print(data1,data2)
     send_command([1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],data1,data2,0,0,0,0)
 def step_without_home(ang):
+    print(ang)
     steps = 1+((ang/360)*steps_per_rev)
-    print(steps)
     steps = int(steps)
-    print(steps)
+    #print(steps)
     data1 = (steps & 65280) >> 8
     data2 = (steps & 255) >> 0
-    print(data1,data2)
+    #print(data1,data2)
     send_command([0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],data1,data2,0,0,0,0)
 def home():
+    print("HOME")
     send_command([0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0],0,0,0,0,0,0)
 def servo_goto(ang):
     ang = ang + 90
     send_command([0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0],0,ang,0,0,0,0)
 def set_laser(state):
     send_command([0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],0,state,0,0,0,0)
+def ping():
+    return send_command([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],0,0,0,0,0,0)
--- display.py
+++ display.py
@@ -6,10 +6,17 @@ import tkinter as tk
 import sys
 import getopt
 import datetime
+#Elena: Added servoofset
+SERVO_OFFSET = 0
 version = "v1.0"
 laser_status = False
 # can have dual tags, keep that in mind, might break stuff later
-args,_ = getopt.getopt(sys.argv[1:], "dhvu", ["debug","help","version","unsafe"])
+port = sys.argv[1]
+control.init(port) # port has to be first command line argument
+args,_ = getopt.getopt(sys.argv[2:], "dhvu", ["debug","help","version","unsafe"])
+f = open("OFFSET",'r')
+OFFSET = int(f.read())
+f.close()
 debug = False
 for argument in args:
     parsed = argument[:1][0]
@@ -27,10 +34,6 @@ for argument in args:
     if parsed == "-v" or parsed == "--version":
         print("version:", version)
         exit()
-    if parsed == "-u" or parsed == "--unsafe":
-        motor_mode = True # false = press = execute release = stop, true = seprate buttons
-    else:
-        motor_mode = False # false = press = execute release = stop, true = seprate buttons
 root = tk.Tk()
 root.title("globe GUI")
 frame = tk.Frame(root)
@@ -108,8 +111,10 @@ while 1:
     time.sleep(1/15)
     owe = 0
     if tracking:
-        prev_loc = [0,0]
+        control.set_laser(1)
+        prev_loc = None
         control.home()
+        control.step_without_home(OFFSET)
         for i in range(10):
             date = datetime.datetime(1,1,1).now()
             index = listbox.curselection()
@@ -118,22 +123,29 @@ while 1:
                 satelite = predict.output[index]
             else:
                 satelite = ['ISS (ZARYA)', 80]
-            print(satelite)
+            #print(satelite)
             loc = predict.track(satelite[1],date)
+            if not prev_loc:
+                prev_loc = [0,0]
             delta_long = loc[1] - prev_loc[1]
             owe += delta_long
-            print("latitude:",loc[0],"longitude:",loc[1])
+            #print("latitude:",loc[0],"longitude:",loc[1])
+            #Elena: Added servoofset
             lat.set("{:.4f}".format(loc[0]))
             long.set("{:.4f}".format(loc[1]))
             if owe > 2:
-                control.step_without_home(-owe)
-                owe -= owe
+                control.step_without_home(owe)
+                owe = 0
+            if owe < -2:
+                control.step_without_home(owe)
+                owe = 0
             print(f"OWE {owe} STEPS")
-            control.servo_goto(int(loc[0]))
+            control.servo_goto(int(loc[0]+SERVO_OFFSET))
             time.sleep(10)
             prev_loc = loc
             root.update_idletasks()
             root.update()
             if not tracking:
+                control.set_laser(0)
                 break
         control.home()
